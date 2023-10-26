df8f623730ae8b98bed69c939a8dd85839549a62
officialtpss <officialtpss@gmail.com>
2023-10-26 11:16:02 +0530

    html upload

---
+++ src/component/Header.js
@@ -0,0 +1,33 @@
+
+function Header() {
+    return (
+        <div className="siteHeader">
+            <div className="siteLogo">
+                <span>OM</span>
+            </div>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Service</a>
+                </li>
+                <li>
+                    <a href="#">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+            <div className="siteLogo">
+                <span>OM</span>
+            </div>
+        </div>
+    )
+}
+
+
+export default Header;
