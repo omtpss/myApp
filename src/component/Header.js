959fd3b7efb2da4459e23aa1beb5a6d7a22a3ee3
officialtpss <officialtpss@gmail.com>
2023-11-08 18:29:04 +0530

    responsive css update

--- src/component/Header.js
+++ src/component/Header.js
@@ -39,8 +39,8 @@ function Header() {
         <>
             <div className={scroll ? "topScroll show" : "topScroll"}>
                 <span onClick={() => scrollToSection('home')}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" className="bi bi-arrow-up-short" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z" />
                     </svg>
                 </span>
             </div>
--- src/component/Marque.js
+++ src/component/Marque.js
@@ -1,12 +1,12 @@
 function Marque() {
 
     const ImpressData = [
-        { text: 'Instead of interrupting, work on attracting', },
-        { text: `Just because you are the loudest, doesn't make you right` },
-        { text: 'Content is king' },
-        { text: 'Master the topic, the message, and the delivery' },
-        { text: 'Either write something worth reading or do something worth writing about' }
-    ]
+        {id:1, text: 'Instead of interrupting, work on attracting', },
+        {id:2, text: 'Just because you are the loudest, does not make you right', },
+        {id:3, text: 'Content is king', },
+        {id:4, text: 'Master the topic, the message, and the delivery', },
+        {id:5, text: 'Either write something worth reading or do something worth writing about', },
+    ];
 
     return (
         <section className="digitalMarque lightBg">
@@ -18,11 +18,11 @@ function Marque() {
                                 {ImpressData.map((data, index) => {
                                     return (
                                         <>
-                                            <span>{ data.text}</span><i>. </i>
+                                            <span key={index}>{data.text}<i>. </i></span>
                                         </>
                                     )
                                 })}
-                            </h1>
+                            </h1> 
                         </div>
                     </div>
                 </div>
